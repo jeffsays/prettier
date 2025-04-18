e349306e7cd6cf955fa34addca17f11a440fcf0c
Jeff Beck <jeff.beck@cumulusds.com>
2025-04-18 10:38:40 -0400

    feat: add prettier-plugin-multiline-arrays

---
+++ src/multiline-arrays.plugin.ts
@@ -0,0 +1,14 @@
+/**
+ * @file - configuration for "prettier-plugin-multiline-arrays"
+ * @author - Jeff Beck
+ * @ref https://www.npmjs.com/package/prettier-plugin-multiline-arrays
+ * @ref https://github.com/electrovir/prettier-plugin-multiline-arrays
+ */
+
+import { type Config } from "prettier";
+
+export const config: Config = {
+  multilineArraysWrapThreshold: 3,
+};
+
+export default config;
